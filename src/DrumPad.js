e6f29a86f2b7fc792e674a385b794f5e4df9debe
Marcus Michaels <hello@modestindustries.co>
2020-02-26 17:17:44 +0000

    Fixed some playback Promise issue, added smaller screen support, and added visual effects

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,32 +1,56 @@
 import React from "react";
 
 class DrumPad extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+
+    this.isPlaying = false;
+    this.audioElement = "";
+  }
 
   componentDidMount() {
+    this.audioElement = document.getElementById(this.props.letter);
     document.addEventListener('keydown', this.handleKeydown);
+    document.addEventListener('transitionend', this.removeClass);
   }
   
   componentWillUnmount() {
     document.removeEventListener('keydown', this.handleKeydown);
+    document.removeEventListener('transitionend', this.removeClass);
   }
   
+  removeClass = (e) => {
+    e.target.classList.remove("active");
+  }
+
   handleKeydown = (e) => {
     if (e.keyCode === this.props.keyCode) {
       this.playSound();
     }
   }
   
+  
+
   playSound() {
-    let drum = document.getElementById(this.props.letter);
+    this.audioElement.onplaying = () => {
+      this.isPlaying = true;
+    }
     
-    drum.currentTime = 0;
-    drum.play();
+    this.audioElement.onpause = () => {
+      this.isPlaying = false;
+    }
+    
+    if (this.isPlaying) {
+      this.audioElement.parentNode.classList.remove("active");
+      this.audioElement.parentNode.classList.add("active");
+      this.audioElement.currentTime = 0;
+      this.audioElement.play();
+    } else {
+      this.audioElement.play();
+      this.audioElement.parentNode.classList.add("active");
+    }
     
     this.props.updateDisplay(this.props.id);
-    
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -14,63 +14,62 @@ class DrumMachine extends React.Component {
           letter: "Q",
           keyCode: 81,
           id:"heater1",
-          audio: "./assets/drums/Heater-1.mp3"
+          audio: process.env.PUBLIC_URL + "/assets/drums/Heater-1.mp3"
         },
         {
           letter: "W",
           keyCode: 87,
           id:"heater2",
-          audio: "./assets/drums/Heater-2.mp3"
+          audio: process.env.PUBLIC_URL + "/assets/drums/Heater-2.mp3"
         },
         {
           letter: "E",
           keyCode: 69,
           id:"heater3",
-          audio: "./assets/drums/Heater-3.mp3"
+          audio: process.env.PUBLIC_URL + "/assets/drums/Heater-3.mp3"
         },
         {
           letter: "A",
           keyCode: 65,
           id:"heater4",
-          audio: "./assets/drums/Heater-4_1.mp3"
+          audio: process.env.PUBLIC_URL + "/assets/drums/Heater-4_1.mp3"
         },
         {
           letter: "S",
           keyCode: 83,
           id:"clap",
-          audio: "./assets/drums/Heater-6.mp3"
+          audio: process.env.PUBLIC_URL + "/assets/drums/Heater-6.mp3"
         },
         {
           letter: "D",
           keyCode: 68,
           id:"open-high-hat",
-          audio: "./assets/drums/Dsc_Oh.mp3"
+          audio: process.env.PUBLIC_URL + "/assets/drums/Dsc_Oh.mp3"
         },
         {
           letter: "Z",
           keyCode: 90,
           id:"kick-hat",
-          audio: "./assets/drums/Kick_n_Hat.mp3"
+          audio: process.env.PUBLIC_URL + "/assets/drums/Kick_n_Hat.mp3"
         },
         {
           letter: "X",
           keyCode: 88,
           id:"kick",
-          audio: "./assets/drums/RP4_KICK_1.mp3"
+          audio: process.env.PUBLIC_URL + "/assets/drums/RP4_KICK_1.mp3"
         },
         {
           letter: "C",
           keyCode: 67,
           id:"closed-high-hat",
-          audio: "./assets/drums/Cev_H2.mp3"
+          audio: process.env.PUBLIC_URL + "/assets/drums/Cev_H2.mp3"
         },
       ],
-      activeSound: ""
+      activeSound: "Here we go!"
     }
   }
 
   displayUpdate = (d) => {
-
     this.setState({
       activeSound: d
     });
