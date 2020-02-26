e3bcfb1e002e8037f15c2c66a6cac1dd5cc2b94b
Marcus Michaels <hello@modestindustries.co>
2020-02-26 12:00:23 +0000

    rebuilt to fix gh-pages issue

---
+++ build/precache-manifest.4c28cb8f11075801cfc9dbda7ab11e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efb32d9e376dcec43c4caa9cc476f7e",
+    "url": "/drumkit/index.html"
+  },
+  {
+    "revision": "f09cb39e7e0908451129",
+    "url": "/drumkit/static/css/main.e751b5ef.chunk.css"
+  },
+  {
+    "revision": "1e0d69b7ff04640d43dd",
+    "url": "/drumkit/static/js/2.8b53bc3c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/drumkit/static/js/2.8b53bc3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09cb39e7e0908451129",
+    "url": "/drumkit/static/js/main.a7f146f3.chunk.js"
+  },
+  {
+    "revision": "c68edc8431a87d42da36",
+    "url": "/drumkit/static/js/runtime-main.57d99a46.js"
+  }
+]);
